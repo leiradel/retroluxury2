98e197d7fdd63156a982a6b049a51058936dd36e
leiradel <andre@leiradella.com>
2021-12-31 17:25:30 +0000

    Fixed djb2

--- src/engine/djb2.c
+++ src/engine/djb2.c
@@ -1,7 +1,7 @@
 #include "djb2.h"
 
-hh2_Djb2Hash hh2_djb2(char const* str) {
-    hh2_Djb2Hash hash = 5381;
+rl2_Djb2Hash rl2_djb2(char const* str) {
+    rl2_Djb2Hash hash = 5381;
 
     while (*str != 0) {
         hash = hash * 33 + (uint8_t)*str++;
--- src/engine/djb2.h
+++ src/engine/djb2.h
@@ -1,12 +1,12 @@
-#ifndef HH2_DJB2_H__
-#define HH2_DJB2_H__
+#ifndef RL2_DJB2_H__
+#define RL2_DJB2_H__
 
 #include <inttypes.h>
 
-#define HH2_PRI_DJB2HASH "0x%08" PRIx32
+#define RL2_PRI_DJB2HASH "0x%08" PRIx32
 
-typedef uint32_t hh2_Djb2Hash;
+typedef uint32_t rl2_Djb2Hash;
 
-hh2_Djb2Hash hh2_djb2(char const* str);
+rl2_Djb2Hash rl2_djb2(char const* str);
 
-#endif // HH2_DJB2_H__
+#endif // RL2_DJB2_H__
