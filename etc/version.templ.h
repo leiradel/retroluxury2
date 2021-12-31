a2c299e04bc4f67c349e0b3d203adc1b6137df13
leiradel <andre@leiradella.com>
2021-12-31 16:26:49 +0000

    Added the version template

---
+++ etc/version.templ.h
@@ -0,0 +1,10 @@
+#ifndef RL2_VERSION_H__
+#define RL2_VERSION_H__
+
+#define RL2_GITHASH "&HASH"
+#define RL2_VERSION "&VERSION"
+#define RL2_DATE "&DATE"
+
+void rl2_logVersions(void);
+
+#endif // RL2_VERSION_H__
