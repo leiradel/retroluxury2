4bd89219e7b8fb0fb294967db1d0767bf8d7cfbb
leiradel <andre@leiradella.com>
2021-12-31 17:10:24 +0000

    Fixed heap header

--- src/engine/heap.h
+++ src/engine/heap.h
@@ -3,7 +3,7 @@
 
 typedef void* (*rl2_Allocf)(void* userdata, void* pointer, size_t size);
 
-void rl2_setAlloc(rl2_Alloc alloc, void* userdata);
+void rl2_setAlloc(rl2_Allocf alloc, void* userdata);
 
 void* rl2_Alloc(size_t size);
 void rl2_Free(void* pointer);
